48f61491cb710b60e224c6587ebdb1bcadb0371e
Hannes Gräuler <hgraeule@uos.de>
2012-07-08 17:59:10 +0200

    Restructured main file

--- dantien.py
+++ dantien.py
@@ -1,42 +1,37 @@
 # -*- coding: utf-8 -*-
 '''
-Dantien
+Dantien application
 '''
-import numpy as np
-import OpenGL.GL as gl
-
-from glumpy import figure, show, Trackball
-from glumpy.graphics import VertexBuffer
-
-from views import Cube, Spectrogram, SeriesPlot, FFTPlot
-from model import TimeSeries
-from feeders import random_sinoids
-
 from itertools import product
+import glumpy
 
-update_rate = 20 # Hz
-size = (512,512)
-ts = TimeSeries(random_sinoids)
+def dantien(feed_func, layout, update_rate=20):
+    ts = TimeSeries(feed_func)
 
-def build_figure(layout):
     cols = len(layout[0])
     rows = len(layout)
 
-    fig = figure(size=size)
+    fig = glumpy.figure()
     for x, y in product(range(cols), range(rows)):
         cons = layout[y][x]
         subfig = fig.add_figure(cols=cols, rows=rows, position=[x,rows-y-1])
         cons(subfig, ts)
-    return fig
 
-def update(_):
-    ts.eat()
-    fig.redraw()
+    @fig.timer(update_rate)
+    def update(_):
+        ts.eat()
+        fig.redraw()
 
-fig = build_figure(layout=[
-        [SeriesPlot],
-        [Spectrogram],
-        [FFTPlot]
-    ])
-fig.timer(update_rate)(update)
-show()
+    glumpy.show()
+
+if __name__ == '__main__':
+    from views import Cube, Spectrogram, SeriesPlot, FFTPlot
+    from model import TimeSeries
+    from feeders import random_sinoids
+
+    dantien(random_sinoids, layout=[
+            [SeriesPlot],
+            [Spectrogram],
+            [FFTPlot]
+        ])
+
